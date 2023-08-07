16d8ffa365f857d02e97b865b451a77c036e73e6
rezha4 <rezha.bahari@gmail.com>
2023-08-07 10:20:55 +0700

    styling & centering everything with CSS
    will tidy up & add color scheme next. after finishing the js.

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("e")
+console.log("h");
+
+let i = 2;
+let j = 2;
+let op = ""
+
+console.log(operator(i, op[0], j));
+
+function operator(i, op, j) {
+    if (op === "+") {
+        return add(i, j);
+    }
+    else if (op === "-") {
+        return subtract(i, j);
+    }
+    else if (op === "*") {
+        return multiply(i, j);
+    }
+    else if (op === "/") {
+        return divide(i, j);
+    }
+}
+
+function add(i, j) {
+    return i + j;
+}
+
+function subtract(i, j) {
+    return i - j;
+}
+
+function multiply(i, j) {
+    return i * j;
+}
+
+function divide(i, j) {
+    return i / j;
+}
