9bc5db77f509bf5aaa932e7afba0ad6184c471a1
rezha4 <rezha.bahari@gmail.com>
2023-08-08 10:52:32 +0700

    finished the logic with a bunch of if else's (maybe there is a more elegant way to do this?)
    need to polish:
    1. display without ","
    2. result need to be always on bottom of display box
    3. need to add a bit more logic on 0 ing the result/variables
    4. also implement those logic on displaying on to display box
    5. polishing the look:
      5a. color scheme/theme (start here)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let j = [];
 let op = [];
 let arr = [];
 let result = 0;
+let Allsymbols = ["c", "=", "+", "-", "*", "/"];
 
 const buttons = document.querySelectorAll("button");
 let displayEqu = document.querySelector(".equation");
@@ -10,14 +11,52 @@ let displayRes = document.querySelector(".result");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (arr.length < 3) {
-            arr.push(button.id);
+        if (arr.length == 0 && Allsymbols.includes(button.id)) {
+            console.log(`NaN --> ${button.id}`);
+            return;
+        }
+        else if (arr.length == 0 && !(Allsymbols.includes(button.id))) {
+            i += button.id;
+            console.log(i);
+            arr[0] = i;
+            console.log(arr);
+        }
+        else if (arr.length == 1 && !(Allsymbols.includes(button.id))) {
+            i += button.id;
+            console.log(i);
+            arr[0] = i;
+            console.log(arr);
+        }
+        else if (arr.length == 1 && Allsymbols.includes(button.id)) {
+            op = button.id;
+            arr[1] = op;
+            console.log(arr);
+        }
+        else if (arr.length == 2 && Allsymbols.includes(button.id)) {
+            op = button.id;
+            arr[1] = op;
+            console.log(arr);
+        }
+        else if (arr.length >= 2 && !(Allsymbols.includes(button.id))) {
+            j += button.id;
+            arr[2] = j;
             console.log(arr)
         }
-        else {
-            result = operator(Number(arr[0]), arr[1], Number(arr[2]));
+        else if (arr.length > 2 && button.id == "=") {
+            result += operator(Number(arr[0]), arr[1], Number(arr[2]));
             arr = [];
+            i = [];
+            op = [];
+            j = [];
         }
+//        if (arr.length < 3 && )
+        // if (arr.length < 3) {
+        //     arr.push(button.id);
+        // }
+        // else {
+        //     result = operator(Number(arr[0]), arr[1], Number(arr[2]));
+        //     arr = [];
+        // }
         displayEqu.textContent = `${arr}`;
         displayRes.textContent = `${result}`;
     })
